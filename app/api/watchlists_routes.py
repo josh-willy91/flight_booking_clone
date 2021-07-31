29348f54c73ccd1a98cec9e6fa596a3353b9a464
josh-willy91 <jwilly91812@yahoo.com>
2021-07-31 12:49:12 -0400

    watchlist backend

--- app/api/watchlists_routes.py
+++ app/api/watchlists_routes.py
@@ -11,36 +11,58 @@ watchlists_routes = Blueprint('watchlists', __name__)
 @watchlists_routes.route('<path:id>')
 @login_required
 def get_watchlists(id):
+    watchlist_data_obj = {}
     watchlists_query = Watchlist.query.filter_by(user_id = id).all()
     watchlist_list = [watchlist.to_dict() for watchlist in watchlists_query]
-
-    print(watchlist_list, '===============================')
+    print(watchlist_list, '========================================')
 
     for watchlist_obj in watchlist_list:
         origin = watchlist_obj['origin']
         destination = watchlist_obj['destination']
         departure_date = watchlist_obj['depart_date']
+        trip_return = watchlist_obj['trip_return']
+        price = watchlist_obj['price']
         num_adults = 1
+        print(origin, destination, departure_date, trip_return, price, num_adults,
+        '==============================================')
 
-        if(watchlist_obj['price'] == None):
-            price = watchlist_obj['price']
-            print(price, '==================skjfs================')
+    #     if(price == None):
+    #         print(price, '================price is none================')
 
-    # amadeus = Client(
-    #     client_id=os.environ.get('API_PUBLIC_KEY'),
-    #     client_secret=os.environ.get('API_SECRET_KEY')
-    # )
-    # response = amadeus.shopping.flight_offers_search.get(
-    #     originLocationCode = origin,
-    #     destinationLocationCode = destination,
-    #     departureDate = departure_date,
-    #     adults = 1,
-    #     currencyCode = 'USD',
-    #     max = 50,
-    # )
-    # print(response.data)
-    # return {'flight': response.data}
+    #         amadeus = Client(
+    #             client_id=os.environ.get('API_PUBLIC_KEY'),
+    #             client_secret=os.environ.get('API_SECRET_KEY')
+    #         )
+    #         response = amadeus.shopping.flight_offers_search.get(
+    #             originLocationCode = origin,
+    #             destinationLocationCode = destination,
+    #             departureDate = departure_date,
+    #             returnDate = trip_return,
+    #             adults = 1,
+    #             currencyCode = 'USD',
+    #             max = 50,
+    #         )
+    #         watchlist_data_obj['watchlist_results'] = response.data
 
+    #     else:
+    #             amadeus = Client(
+    #             client_id=os.environ.get('API_PUBLIC_KEY'),
+    #             client_secret=os.environ.get('API_SECRET_KEY')
+    #         )
+    #             response = amadeus.shopping.flight_offers_search.get(
+    #                 originLocationCode = origin,
+    #                 destinationLocationCode = destination,
+    #                 departureDate = departure_date,
+    #                 returnDate = trip_return,
+    #                 maxPrice = price,
+    #                 adults = 1,
+    #                 currencyCode = 'USD',
+    #                 max = 50,
+    #             )
+    #             watchlist_data_obj['watchlist_results_price'] = response.data
+
+    # watchlist_data_obj['watchlist_list'] = watchlist_list
+    # print(watchlist_data_obj, '=====================================================')
     return {'watchlist_list': watchlist_list}
 
 
