49019bc2fd9c563475f2e759489587a36d0caaea
josh-willy91 <jwilly91812@yahoo.com>
2021-08-02 12:59:06 -0400

    styling

--- react-app/src/components/Home.js
+++ react-app/src/components/Home.js
@@ -97,8 +97,8 @@ function Home() {
 
     return (
         <div className='homeDiv'>
+        <img src="images/searchBackground.jpg" alt="" className="searchBackground"/>
             <div className='searchDiv'>
-                <img src="/images/bookYeah - searchBackground.jpg" alt="" className="searchBackground"/>
                 <h3>Search Flights</h3>
                 <form onSubmit={searchFlights} className='searchForm'>
                     <div className='searchInputDivOrigin'>
