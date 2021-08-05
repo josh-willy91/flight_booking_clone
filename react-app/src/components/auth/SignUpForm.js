6d85c92d5446eaf754e96c0902bf86af547bd37f
josh-willy91 <jwilly91812@yahoo.com>
2021-08-05 17:43:16 -0400

    getting close to final prouct

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -57,7 +57,7 @@ const SignUpForm = () => {
   return (
     <div className='signupDiv'>
       <img className='signupImg' src="images/signup.jpg" alt=""/>
-      <div>
+      <div className='form_wrapper'>
         <form className='signupForm' onSubmit={onSignUp}>
           <h3>Signup</h3>
           <div>
