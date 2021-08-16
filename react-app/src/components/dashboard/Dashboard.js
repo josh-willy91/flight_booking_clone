1c05e993b80d78abef5392795f6c3f6f9e2306d6
josh-willy91 <jwilly91812@yahoo.com>
2021-08-16 13:01:06 -0400

    added WatchlistSearch component

--- react-app/src/components/dashboard/Dashboard.js
+++ react-app/src/components/dashboard/Dashboard.js
@@ -6,16 +6,17 @@ import differenceInCalendarDays from 'date-fns/differenceInCalendarDays'
 import WatchlistForm from './WatchlistForm';
 import ModalWatchlistForm from './ModalWatchlistForm';
 import Bookings from './Bookings';
+import WatchlistSearch from './WatchlistSearch';
 import '../../styles/dashboard.css'
 
 
 function Dashboard() {
   const dispatch = useDispatch()
-  const { userId }  = useParams();
+  const { userId } = useParams();
 
   const sessionUser = useSelector((state) => state.session.user)
-  const bookings = useSelector((state) => state.dashboardReducer.bookings)
-  const watchlists = useSelector((state) => state.dashboardReducer.watchlists)
+  // const bookings = useSelector((state) => state.dashboardReducer.bookings)
+  // const watchlists = useSelector((state) => state.dashboardReducer.watchlists)
 
   const [user, setUser] = useState({});
   const [flightId, setFlightId] = useState('');
@@ -45,39 +46,17 @@ function Dashboard() {
     dispatch(watchlistDetails(userId))
   }, [])
 
-  useEffect(async() => {
-    if(flightId) {
-      await dispatch(deleteOneBooking({flightId}))
+  useEffect(async () => {
+    if (flightId) {
+      await dispatch(deleteOneBooking({ flightId }))
       window.location.reload();
     }
   }, [flightId])
 
-  const deleteWatchlist = async(watchlistId) => {
+  const deleteWatchlist = async (watchlistId) => {
     await dispatch(deleteOneWatchlist(watchlistId))
     window.location.reload();
   }
-
-  const getLastIATA = (array) => {
-    const nested = array.itineraries[0].segments
-    const length = nested.length - 1;
-    return nested[length].arrival.iataCode
-  }
-
-  const getLastDeparture = (array) => {
-    const nested = array.itineraries[0].segments
-    const length = nested.length - 1;
-    return nested[length].departure.at
-  }
-
-
-  // formatISO(date, [options]) syntax for function
-  // formats the data string returned from query
-  // ('+020201-06-26T00:00:00.000Z')
-  const format = function(dateString) {
-    const spitTime = dateString.split('T').join(' ')
-    // const result = format(spitTime, 'eeee do MMMM')
-    return spitTime;
-  };
 
   // const showHideElements = () => {
   //   let show = document.getElementById('showHide')
@@ -92,83 +71,22 @@ function Dashboard() {
   // }
 
 
-  if(!sessionUser) {
+  if (!sessionUser) {
     return null;
   }
   return (
     <div className='dashboardDiv'>
-      <Bookings setFlightId={setFlightId}/>
+      <Bookings setFlightId={setFlightId} />
       <div className='watchlistsDiv'>
         <WatchlistForm origin={origin} setOrigin={setOrigin} destination={destination}
-        setDestination={setDestination} price={price} setPrice={setPrice} start={start}
-        setStart={setStart} tripReturn={tripReturn} setTripReturn={setTripReturn}/>
+          setDestination={setDestination} price={price} setPrice={setPrice} start={start}
+          setStart={setStart} tripReturn={tripReturn} setTripReturn={setTripReturn} />
         <ModalWatchlistForm origin={origin} setOrigin={setOrigin} destination={destination}
-        setDestination={setDestination} price={price} setPrice={setPrice} start={start}
-        setStart={setStart} tripReturn={tripReturn} setTripReturn={setTripReturn}
-        watchlistId={watchlistId} openModal={openModal} closeModal={() => setOpenModal(false)}/>
-        <div className='watchlistsSearchDiv'>
-          <ul className='watchlistsSearchUl'>
-            {watchlists && watchlists.watchlist_list.map((details) => (
-              <li className='watchlistsSearchLi' key={details.id}>
-                <div className='searchCriteriaDiv'>
-                  <h3>Watchlist Search Criteria</h3>
-                  <div>Leaving from {details.origin}</div>
-                  <div>Departure Date: {details.depart_date}</div>
-                  <div>Destination: {details.destination}</div>
-                  <div>Return flight leaves: {details.trip_return}</div>
-                  <div>Price: {details.price ? `Less than $${details.price}`: 'No limit set'}</div>
-                  <div>
-                    <button onClick={() => deleteWatchlist(details.id)}>Delete Watchlist</button>
-                    <button onClick={() => {
-                      setOpenModal(true)
-                      setWatchlistId(details.id)}
-                    }> Edit Watchlist</button>
-                  </div>
-                </div>
-                <div className='carousel'>
-                  {watchlists.watchlist_data_obj[`${details.id}`].map((flight) => (
-                    <li className='carousel_item' key={flight.id}>
-                      <div>
-                          {flight.oneWay === true ?
-                          <p>Flight Route {flight.itineraries[0].segments[0].departure.iataCode} to {flight.itineraries[0].segments[0].arrival.iataCode}</p> :
-                          <p>Flight Route {flight.itineraries[0].segments[0].departure.iataCode} to {getLastIATA(flight)}</p>
-                          }
-                      </div>
-                      <div>
-                          {flight.oneWay === true ?
-                          <p>One Way: Yes</p> :
-                          <p>Layovers: {flight.itineraries[0].segments.length - 1}</p>
-                          }
-                      </div>
-                      <div>
-                          <p>Departs: {format(flight.itineraries[0].segments[0].departure.at)}</p>
-                      </div>
-                      <div>
-                          <p>Arrival: {format(flight.itineraries[0].segments[0].arrival.at)}</p>
-                      </div>
-                      <div>
-                          <p>Return Flight: {format(getLastDeparture(flight))}</p>
-                      </div>
-                      <div>
-                          <p>Price: ${flight.price.total}</p>
-                      </div>
-                      <div>
-                          <p>Airline Code: {flight.validatingAirlineCodes[0]}</p>
-                      </div>
-                      <div>
-                          <p>Flight Number: {flight.validatingAirlineCodes[0]}{flight.itineraries[0].segments[0].number}</p>
-                      </div>
-                    </li>
-                  ))}
-                </div>
-                {/* <div className='carousel_actions'>
-                  <button id='carousel_Button--Prev' aria-label='previous slide'>Previous Slide</button>
-                  <button id='carousel_Button--Next' aria-label='next slide'>Next Slide</button>
-                </div> */}
-              </li>
-            ))}
-          </ul>
-        </div>
+          setDestination={setDestination} price={price} setPrice={setPrice} start={start}
+          setStart={setStart} tripReturn={tripReturn} setTripReturn={setTripReturn}
+          watchlistId={watchlistId} openModal={openModal} closeModal={() => setOpenModal(false)} />
+        <WatchlistSearch setOpenModal={setOpenModal} setWatchlistId={setWatchlistId}
+          deleteWatchlist={deleteWatchlist}/>
       </div>
     </div>
   );
---
+++ react-app/src/components/dashboard/WatchlistSearch.js
@@ -0,0 +1,101 @@
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { deleteOneWatchlist } from '../../store/dashboard'
+
+
+
+function WatchlistSearch({setOpenModal, setWatchlistId, deleteWatchlist}) {
+
+    const watchlists = useSelector((state) => state.dashboardReducer.watchlists)
+
+    const getLastDeparture = (array) => {
+        const nested = array.itineraries[0].segments
+        const length = nested.length - 1;
+        return nested[length].departure.at
+    }
+
+    const getLastIATA = (array) => {
+        const nested = array.itineraries[0].segments
+        const length = nested.length - 1;
+        return nested[length].arrival.iataCode
+    }
+
+      // formatISO(date, [options]) syntax for function
+    // formats the data string returned from query
+    // ('+020201-06-26T00:00:00.000Z')
+    const format = function (dateString) {
+        const spitTime = dateString.split('T').join(' ')
+        // const result = format(spitTime, 'eeee do MMMM')
+        return spitTime;
+    };
+
+
+    return (
+        <div className='watchlistsSearchDiv'>
+            <ul className='watchlistsSearchUl'>
+                {watchlists && watchlists.watchlist_list.map((details) => (
+                    <li className='watchlistsSearchLi' key={details.id}>
+                        <div className='searchCriteriaDiv'>
+                            <h3>Watchlist Search Criteria</h3>
+                            <div>Leaving from {details.origin}</div>
+                            <div>Departure Date: {details.depart_date}</div>
+                            <div>Destination: {details.destination}</div>
+                            <div>Return flight leaves: {details.trip_return}</div>
+                            <div>Price: {details.price ? `Less than $${details.price}` : 'No limit set'}</div>
+                            <div>
+                                <button onClick={() => deleteWatchlist(details.id)}>Delete Watchlist</button>
+                                <button onClick={() => {
+                                    setOpenModal(true)
+                                    setWatchlistId(details.id)
+                                }
+                                }> Edit Watchlist</button>
+                            </div>
+                        </div>
+                        <div className='carousel'>
+                            {watchlists.watchlist_data_obj[`${details.id}`].map((flight) => (
+                                <li className='carousel_item' key={flight.id}>
+                                    <div>
+                                        {flight.oneWay === true ?
+                                            <p>Flight Route {flight.itineraries[0].segments[0].departure.iataCode} to {flight.itineraries[0].segments[0].arrival.iataCode}</p> :
+                                            <p>Flight Route {flight.itineraries[0].segments[0].departure.iataCode} to {getLastIATA(flight)}</p>
+                                        }
+                                    </div>
+                                    <div>
+                                        {flight.oneWay === true ?
+                                            <p>One Way: Yes</p> :
+                                            <p>Layovers: {flight.itineraries[0].segments.length - 1}</p>
+                                        }
+                                    </div>
+                                    <div>
+                                        <p>Departs: {format(flight.itineraries[0].segments[0].departure.at)}</p>
+                                    </div>
+                                    <div>
+                                        <p>Arrival: {format(flight.itineraries[0].segments[0].arrival.at)}</p>
+                                    </div>
+                                    <div>
+                                        <p>Return Flight: {format(getLastDeparture(flight))}</p>
+                                    </div>
+                                    <div>
+                                        <p>Price: ${flight.price.total}</p>
+                                    </div>
+                                    <div>
+                                        <p>Airline Code: {flight.validatingAirlineCodes[0]}</p>
+                                    </div>
+                                    <div>
+                                        <p>Flight Number: {flight.validatingAirlineCodes[0]}{flight.itineraries[0].segments[0].number}</p>
+                                    </div>
+                                </li>
+                            ))}
+                        </div>
+                        {/* <div className='carousel_actions'>
+                            <button id='carousel_Button--Prev' aria-label='previous slide'>Previous Slide</button>
+                            <button id='carousel_Button--Next' aria-label='next slide'>Next Slide</button>
+                        </div> */}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default WatchlistSearch;
