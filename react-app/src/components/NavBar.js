eeee1170c443d620c8229a5ef0357a7c53867e4f
josh-willy91 <jwilly91812@yahoo.com>
2021-08-24 19:09:19 -0400

    eod

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -15,12 +15,14 @@ const NavBar = () => {
         <ul className='navbarUl'>
           <h2 className='logo'>BookYeah</h2>
           <div className='navBarLeft'>
-            <NavLink className='navLink' className='middle' to='/bookyeah' exact={true} activeClassName='active'>
+            <NavLink className='navLink' className='middle'
+              to='/bookyeah' exact={true} activeClassName='active'>
               Home
             </NavLink>
           </div>
           <div className='navBarLeft'>
-            <NavLink className='navLink' className='left' to={`/users/${sessionUser.id}`} exact={true} activeClassName='active'>
+            <NavLink className='navLink' className='left'
+              to={`/users/${sessionUser.id}`} exact={true} activeClassName='active'>
               Dashboard
             </NavLink>
           </div>
@@ -35,17 +37,20 @@ const NavBar = () => {
       <nav className='navbar'>
         <ul className='navbarUl'>
           <div className='navBarLeft'>
-            <NavLink className='navLink' className='middle' to='/bookyeah' exact={true} activeClassName='active'>
+            <NavLink className='navLink' className='middle'
+              to='/bookyeah' exact={true} activeClassName='active'>
               Home
             </NavLink>
           </div>
           <div className='navBarLeft'>
-            <NavLink className='navLink' className='left' to='/login' exact={true} activeClassName='active'>
+            <NavLink className='navLink' className='left'
+              to='/login' exact={true} activeClassName='active'>
               Login
             </NavLink>
           </div>
           <div className='navBarLeft'>
-            <NavLink className='navLink' className='right' to='/sign-up' exact={true} activeClassName='active'>
+            <NavLink className='navLink' className='right'
+              to='/sign-up' exact={true} activeClassName='active'>
               Sign Up
             </NavLink>
           </div>
