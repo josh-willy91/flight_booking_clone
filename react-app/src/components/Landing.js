0e171113ea004b9459de6c4636f33d7bb0491403
josh-willy91 <jwilly91812@yahoo.com>
2021-08-25 12:23:21 -0400

    landing style

--- react-app/src/components/Landing.js
+++ react-app/src/components/Landing.js
@@ -15,17 +15,17 @@ function Landing() {
     }
 
     return (
-        <div className='landingDiv'>
+        <div className='landing-wrapper'>
             <div className='landingContent'>
                 <h3>Welcome to BookYeah!</h3>
-                <p>We make finding flights easier for you
+                <p className='landing-p'><strong>We make finding flights easier </strong>
                     so you don't have to worry with the
                     hastle of searching for the best price
                     or losing your flight booking information.
                 </p>
-                <p>
-                    What are you waiting for??? Exotic places
-                    and amzing adventures await you ahead!
+                <p className='landing-p'>
+                    What are you waiting for??? <strong>Exotic places </strong>
+                    and <strong>amzing adventures </strong>await you ahead!
                     signup or login and start booking flights.
                 </p>
                 <div className='buttonDiv'>
@@ -33,6 +33,18 @@ function Landing() {
                     <button onClick={loginClick}>Login</button>
                 </div>
             </div>
+            <img className='img-1' src='images/landing-1.jpg'></img>
+            <img className='img-2' src='images/landing-2.jpg'></img>
+            <img className='img-3' src='images/landing-3.jpg'></img>
+            <img className='img-4' src='images/landing-4.jpg'></img>
+            <img className='img-5' src='images/landing-5.jpg'></img>
+            <img className='img-6' src='images/landing-6.jpg'></img>
+            <div className='T'>T</div>
+            <div className='R'>R</div>
+            <div className='A'>A</div>
+            <div className='V'>V</div>
+            <div className='E'>E</div>
+            <div className='L'>L</div>
         </div>
     )
 }
