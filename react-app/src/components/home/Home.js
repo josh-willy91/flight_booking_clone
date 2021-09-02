a72bb25b060c1d43d774baa5582373780f7eb3a0
josh-willy91 <jwilly91812@yahoo.com>
2021-09-02 17:17:35 -0400

    loading animation for search

--- react-app/src/components/home/Home.js
+++ react-app/src/components/home/Home.js
@@ -49,32 +49,33 @@ function Home() {
                     :
                     <div>
                         {loading ?
-                            <div className='loading'>loading</div>
-                            :
                             <section className='loading-wrapper'>
-                                <div className='loading'>loading
-                                    {/* <span style='--i:1;'></span>
-                                    <span style='--i:2;'></span>
-                                    <span style='--i:3;'></span>
-                                    <span style='--i:4;'></span>
-                                    <span style='--i:5;'></span>
-                                    <span style='--i:6;'></span>
-                                    <span style='--i:7;'></span>
-                                    <span style='--i:8;'></span>
-                                    <span style='--i:9;'></span>
-                                    <span style='--i:10;'></span>
-                                    <span style='--i:11;'></span>
-                                    <span style='--i:12;'></span>
-                                    <span style='--i:13;'></span>
-                                    <span style='--i:14;'></span>
-                                    <span style='--i:15;'></span>
-                                    <span style='--i:16;'></span>
-                                    <span style='--i:17;'></span>
-                                    <span style='--i:18;'></span>
-                                    <span style='--i:19;'></span>
-                                    <span style='--i:20;'></span> */}
+                                <div className='loading'>
+                                    <span className='p1'></span>
+                                    <span className='p2'></span>
+                                    <span className='p3'></span>
+                                    <span className='p4'></span>
+                                    <span className='p5'></span>
+                                    <span className='p6'></span>
+                                    <span className='p7'></span>
+                                    <span className='p8'></span>
+                                    <span className='p9'></span>
+                                    <span className='p10'></span>
+                                    <span className='p11'></span>
+                                    <span className='p12'></span>
+                                    <span className='p13'></span>
+                                    <span className='p14'></span>
+                                    <span className='p15'></span>
+                                    <span className='p16'></span>
+                                    <span className='p17'></span>
+                                    <span className='p18'></span>
+                                    <span className='p19'></span>
+                                    <span className='p20'></span>
                                 </div>
-                            </section>}
+                            </section>
+                            :
+                            <div className='searchResults-h3'>Search Results</div>
+                        }
                         {/* <h3 className='searchResults-h3'>Search Results</h3> */}
                         <ul className='searchResultsUl'>
                             {searchResults && searchResults.flight.map((flightDetails) => (
